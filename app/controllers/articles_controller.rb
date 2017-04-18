b03026fc87229ec5bf89dcba84d27193ca8f8ecd
Tranducthang28011996 <tieuhodo96@gmail.com>
2017-04-19 00:41:39 +0700

    fix code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,42 +3,27 @@ class ArticlesController < ApplicationController
 	require 'nokogiri'
 	require 'open-uri'
   def index
-  	@keyword = params[:gettext].downcase
-   	if @keyword.empty?
-  		@load2=[]
-  	else
-	  	link_page_soha="http://tratu.soha.vn/dict/en_vn/"
-	  	get_link_soha = link_page_soha + params[:gettext].downcase
-		page = Nokogiri::HTML(open(get_link_soha))
-		soha = Array.new
-		soha = page.css("div#content-3.section-h3 span")
-		@load2 = soha
-		@load1 = page.css("span.mw-headline b")
-		# byebug
-	end
-	#load image from Google
-	uri = URI("https://www.google.co.kr/search?q="+params[:gettext]+"&es_sm=93&source=lnms&tbm=isch&sa=X&ved=0CAcQ_AUoAWoVChMIxs2HlMH4xwIVouWmCh1nigKU&biw=1600&bih=775")
-	html_doc = Nokogiri::HTML(open(uri))
-	@images = html_doc.css("a > img")
   end
-
+  
   def getdata
-  	@keyword = params[:gettext].downcase
-   	if @keyword.empty?
-  		@load2=[]
+  	@value = params[:search].downcase	
+   	if @value == nil
+  		
   	else
 	  	link_page_soha="http://tratu.soha.vn/dict/en_vn/"
-	  	get_link_soha = link_page_soha + params[:gettext].downcase
+	  	get_link_soha = link_page_soha + params[:search].downcase
 		page = Nokogiri::HTML(open(get_link_soha))
 		soha = Array.new
-		soha = page.css("div#content-3.section-h3 span")
+		soha = page.css("div#content-5 h5")
 		@load2 = soha
-		@load1 = page.css("span.mw-headline b")
 		# byebug
+		uri = URI("https://www.google.co.kr/search?q="+params[:search]+"&es_sm=93&source=lnms&tbm=isch&sa=X&ved=0CAcQ_AUoAWoVChMIxs2HlMH4xwIVouWmCh1nigKU&biw=1600&bih=775")
+		html_doc = Nokogiri::HTML(open(uri))
+		@images = html_doc.css("a > img")
+		render :layout => false
+
+		
 	end
-	uri = URI("https://www.google.co.kr/search?q="+params[:gettext]+"&es_sm=93&source=lnms&tbm=isch&sa=X&ved=0CAcQ_AUoAWoVChMIxs2HlMH4xwIVouWmCh1nigKU&biw=1600&bih=775")
-	html_doc = Nokogiri::HTML(open(uri))
-	@images = html_doc.css("a > img")
-	render :layout => false
+	
   end
 end
